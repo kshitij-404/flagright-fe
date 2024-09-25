e4d93e2f846d7b908a3c37a515dbe2587e84cf84
Kshitij Ayush <kshitij4ka@gmail.com>
2024-09-25 20:32:28 +0530

    fix: improve sidbar layout

--- src/components/TransactionDetails/TransactionDetails.tsx
+++ src/components/TransactionDetails/TransactionDetails.tsx
@@ -2,15 +2,14 @@ import dayjs from 'dayjs';
 import { useEffect, useState } from 'react';
 import {
   Badge,
+  Box,
   CopyButton,
-  Divider,
   Drawer,
   Flex,
-  Group,
   NumberFormatter,
   Paper,
+  SegmentedControl,
   SimpleGrid,
-  Tabs,
   Text,
   Title,
   Tooltip,
@@ -19,6 +18,8 @@ import { useDisclosure } from '@mantine/hooks';
 import { BASE_URL } from '@/utils/constants';
 
 import './TransactionDetails.modules.css';
+
+import { IconArrowRight } from '@tabler/icons-react';
 
 interface TransactionDetailsDrawerProps {
   transactionId: string | null;
@@ -31,6 +32,7 @@ export function TransactionDetailsDrawer({
 }: TransactionDetailsDrawerProps) {
   const [opened, { open, close }] = useDisclosure(false);
   const [transaction, setTransaction] = useState<any>(null);
+  const [activeTab, setActiveTab] = useState('originDevice');
 
   useEffect(() => {
     if (transactionId) {
@@ -48,68 +50,112 @@ export function TransactionDetailsDrawer({
     <SimpleGrid cols={2} spacing="sm">
       {deviceData.batteryLevel !== undefined && (
         <>
-          <Text c="dimmed">Battery Level:</Text>
-          <Text>{deviceData.batteryLevel}%</Text>
+          <Text c="dimmed" size="sm">
+            Battery Level:
+          </Text>
+          <Text size="sm" ta="right">
+            {deviceData.batteryLevel}%
+          </Text>
         </>
       )}
       {deviceData.deviceLatitude !== undefined && (
         <>
-          <Text c="dimmed">Latitude:</Text>
-          <Text>{deviceData.deviceLatitude}</Text>
+          <Text c="dimmed" size="sm">
+            Latitude:
+          </Text>
+          <Text size="sm" ta="right">
+            {deviceData.deviceLatitude}
+          </Text>
         </>
       )}
       {deviceData.deviceLongitude !== undefined && (
         <>
-          <Text c="dimmed">Longitude:</Text>
-          <Text>{deviceData.deviceLongitude}</Text>
+          <Text c="dimmed" size="sm">
+            Longitude:
+          </Text>
+          <Text size="sm" ta="right">
+            {deviceData.deviceLongitude}
+          </Text>
         </>
       )}
       {deviceData.ipAddress && (
         <>
-          <Text c="dimmed">IP Address:</Text>
-          <Text>{deviceData.ipAddress}</Text>
+          <Text c="dimmed" size="sm">
+            IP Address:
+          </Text>
+          <Text size="sm" ta="right">
+            {deviceData.ipAddress}
+          </Text>
         </>
       )}
       {deviceData.deviceIdentifier && (
         <>
-          <Text c="dimmed">Device Identifier:</Text>
-          <Text>{deviceData.deviceIdentifier}</Text>
+          <Text c="dimmed" size="sm">
+            Device Identifier:
+          </Text>
+          <Text size="sm" ta="right">
+            {deviceData.deviceIdentifier}
+          </Text>
         </>
       )}
       {deviceData.vpnUsed !== undefined && (
         <>
-          <Text c="dimmed">VPN Used:</Text>
-          <Text>{deviceData.vpnUsed ? 'Yes' : 'No'}</Text>
+          <Text c="dimmed" size="sm">
+            VPN Used:
+          </Text>
+          <Text size="sm" ta="right">
+            {deviceData.vpnUsed ? 'Yes' : 'No'}
+          </Text>
         </>
       )}
       {deviceData.operatingSystem && (
         <>
-          <Text c="dimmed">Operating System:</Text>
-          <Text>{deviceData.operatingSystem}</Text>
+          <Text c="dimmed" size="sm">
+            Operating System:
+          </Text>
+          <Text size="sm" ta="right">
+            {deviceData.operatingSystem}
+          </Text>
         </>
       )}
       {deviceData.deviceMaker && (
         <>
-          <Text c="dimmed">Device Maker:</Text>
-          <Text>{deviceData.deviceMaker}</Text>
+          <Text c="dimmed" size="sm">
+            Device Maker:
+          </Text>
+          <Text size="sm" ta="right">
+            {deviceData.deviceMaker}
+          </Text>
         </>
       )}
       {deviceData.deviceModel && (
         <>
-          <Text c="dimmed">Device Model:</Text>
-          <Text>{deviceData.deviceModel}</Text>
+          <Text c="dimmed" size="sm">
+            Device Model:
+          </Text>
+          <Text size="sm" ta="right">
+            {deviceData.deviceModel}
+          </Text>
         </>
       )}
       {deviceData.deviceYear && (
         <>
-          <Text c="dimmed">Device Year:</Text>
-          <Text>{deviceData.deviceYear}</Text>
+          <Text c="dimmed" size="sm">
+            Device Year:
+          </Text>
+          <Text size="sm" ta="right">
+            {deviceData.deviceYear}
+          </Text>
         </>
       )}
       {deviceData.appVersion && (
         <>
-          <Text c="dimmed">App Version:</Text>
-          <Text>{deviceData.appVersion}</Text>
+          <Text c="dimmed" size="sm">
+            App Version:
+          </Text>
+          <Text size="sm" ta="right">
+            {deviceData.appVersion}
+          </Text>
         </>
       )}
       {/* {deviceData.deviceLatitude !== undefined && deviceData.deviceLongitude !== undefined && (
@@ -143,7 +189,7 @@ export function TransactionDetailsDrawer({
           <Title order={3} fz={20}>
             Transaction Details
           </Title>
-          <CopyButton value={transaction.transactionId} timeout={1000}>
+          <CopyButton value={transaction?.transactionId} timeout={1000}>
             {({ copied, copy }) => (
               <Tooltip label={copied ? 'Copied' : 'Copy'} withArrow position="right">
                 <Badge
@@ -155,7 +201,7 @@ export function TransactionDetailsDrawer({
                   }}
                   // size="lg"
                 >
-                  {transaction.transactionId}
+                  {transaction?.transactionId}
                 </Badge>
               </Tooltip>
             )}
@@ -167,113 +213,181 @@ export function TransactionDetailsDrawer({
       offset={10}
     >
       {transaction ? (
-        <Flex gap={20} direction="column">
+        <Flex gap={20} direction="column" pb={32}>
           <Paper p={14} radius={20} bg="gray.0">
             <Flex
               justify="space-between"
               align="center"
               // style={{ border: '1px solid #eaeaea', padding: '14px', borderRadius: '20px' }}
             >
-              <Text size="md" className="font-mono">
+              {/* <Text size="md" className="font-mono"> */}
+              <Flex className="font-mono" align="center" gap={10}>
                 {/* From{' '} */}
-                <Badge className="font-mono" variant="light" size="lg" color="red" mx={3}>
+                <Badge className="font-mono" variant="light" size="lg" color="red">
                   {transaction.originUserId}
                 </Badge>
-                {' => '}
+                <IconArrowRight size={20} stroke={1.5} />
+                {/* {' => '} */}
                 {/* <br /> */}
                 {/* to{' '} */}
-                <Badge className="font-mono" variant="light" size="lg" color="teal" mx={3}>
+                <Badge className="font-mono" variant="light" size="lg" color="teal">
                   {transaction.destinationUserId}
                 </Badge>
-              </Text>
+              </Flex>
+              {/* </Text> */}
               <Text size="lg">
                 <strong>
                   <NumberFormatter
                     value={transaction.originAmountDetails.transactionAmount}
                     thousandSeparator
                     decimalScale={2}
-                    prefix={`${transaction.originAmountDetails.transactionCurrency} `}
+                    suffix={` ${transaction.originAmountDetails.transactionCurrency}`}
                   />
                 </strong>
               </Text>
             </Flex>
           </Paper>
 
-          <Group mt="md">
-            Type:
-            <Badge color="blue" size="lg">
-              {transaction.type}
-            </Badge>
-            State:
-            <Badge color="green" size="lg">
-              {transaction.transactionState}
-            </Badge>
-          </Group>
-          <Divider my="sm" />
-          <Tabs variant="outline" defaultValue="details">
-            <Tabs.List>
-              <Tabs.Tab value="details">Details</Tabs.Tab>
-              <Tabs.Tab value="originDevice">Origin Device</Tabs.Tab>
-              <Tabs.Tab value="destinationDevice">Destination Device</Tabs.Tab>
-            </Tabs.List>
-            <Tabs.Panel value="details" pt="xs">
-              <Text>Description:</Text>
-              <Text
-                c="dimmed"
-                mt={12}
-                mb={12}
-                style={{ border: '1px solid #eaeaea', padding: '14px', borderRadius: '20px' }}
+          <Flex direction="column" gap={12}>
+            <Flex direction="column" gap={6}>
+              <Title order={5} fw={600}>
+                Overview:
+              </Title>
+              {/* <Paper>
+                <Text
+                  c="dimmed"
+                  size="sm"
+                  // style={{ border: '1px solid #eaeaea', padding: '14px', borderRadius: '20px' }}
+                >
+                  {transaction.description}
+                </Text>
+              </Paper> */}
+            </Flex>
+            <Box
+              display="grid"
+              style={{
+                gridTemplateColumns: 'auto 1fr',
+                gap: 10,
+              }}
+            >
+              <Text c="dimmed" size="sm">
+                Type:
+              </Text>
+              <Badge
+                variant="light"
+                // p={0}
+                color="blue"
+                size="md"
+                ml="auto"
               >
+                {transaction.type}
+              </Badge>
+              <Text c="dimmed" size="sm">
+                State:
+              </Text>
+              <Badge
+                variant="light"
+                // p={0}
+                color="indigo"
+                size="md"
+                ml="auto"
+              >
+                {transaction.transactionState}
+              </Badge>
+              <Text c="dimmed" size="sm">
+                Description:
+              </Text>
+              <Text ta="right" size="sm" fw={500}>
                 {transaction.description}
               </Text>
-              <Group mt="md" mb="md">
-                {transaction.tags.map((tag: any) => (
-                  <Badge key={tag._id} color="green">
-                    {`${tag.key}: ${tag.value}`}
-                  </Badge>
-                ))}
-              </Group>
-              <SimpleGrid cols={2} spacing="sm">
-                <Text c="dimmed">Time:</Text>
-                <Text
-                  style={{ textAlign: 'right' }}
-                >{`${dayjs(transaction.timestamp).format('MMM D, YYYY')} at ${dayjs(transaction.timestamp).format('h:mm A')}`}</Text>
-                <Text c="dimmed">Promotion Code Used:</Text>
-                <Text style={{ textAlign: 'right' }}>
-                  {transaction.promotionCodeUsed ? 'Yes' : 'No'}
-                </Text>
-                <Text c="dimmed">Reference:</Text>
-                <Text style={{ textAlign: 'right' }}>{transaction.reference}</Text>
-                <Text c="dimmed">Origin Amount:</Text>
-                <Text style={{ textAlign: 'right' }}>
-                  <NumberFormatter
-                    value={transaction.originAmountDetails.transactionAmount}
-                    thousandSeparator
-                    decimalScale={2}
-                    prefix={`${transaction.originAmountDetails.transactionCurrency} `}
-                  />
-                </Text>
-                <Text c="dimmed">Destination Amount:</Text>
-                <Text style={{ textAlign: 'right' }}>
-                  <NumberFormatter
-                    value={transaction.destinationAmountDetails.transactionAmount}
-                    thousandSeparator
-                    decimalScale={2}
-                    prefix={`${transaction.destinationAmountDetails.transactionCurrency} `}
-                  />
-                </Text>
-              </SimpleGrid>
-            </Tabs.Panel>
-            <Tabs.Panel value="originDevice" pt="xs">
-              {renderDeviceDetails(transaction.originDeviceData)}
-            </Tabs.Panel>
-            <Tabs.Panel value="destinationDevice" pt="xs">
-              {renderDeviceDetails(transaction.destinationDeviceData)}
-            </Tabs.Panel>
-          </Tabs>
+              <Text c="dimmed" size="sm">
+                Tags:
+              </Text>
+              <Text ta="right" size="sm" fw={500}>
+                {transaction.tags.map((tag: any) => tag.key).join(', ')}
+              </Text>
+              <Text c="dimmed" size="sm">
+                Time:
+              </Text>
+              <Text
+                ta="right"
+                size="sm"
+                fw={500}
+              >{`${dayjs(transaction.timestamp).format('MMM D, YYYY')} at ${dayjs(transaction.timestamp).format('h:mm A')}`}</Text>
+              <Text c="dimmed" size="sm">
+                Promotion Code Used:
+              </Text>
+              <Text ta="right" size="sm" fw={500}>
+                {transaction.promotionCodeUsed ? 'Yes' : 'No'}
+              </Text>
+              <Text c="dimmed" size="sm">
+                Reference:
+              </Text>
+              <Text ta="right" size="sm" fw={500}>
+                {transaction.reference}
+              </Text>
+              <Text c="dimmed" size="sm">
+                Origin Amount:
+              </Text>
+              <Text ta="right" size="sm" fw={500}>
+                <NumberFormatter
+                  value={transaction.originAmountDetails.transactionAmount}
+                  thousandSeparator
+                  decimalScale={2}
+                  prefix={`${transaction.originAmountDetails.transactionCurrency} `}
+                />
+              </Text>
+              <Text c="dimmed" size="sm">
+                Destination Amount:
+              </Text>
+              <Text ta="right" size="sm" fw={500}>
+                <NumberFormatter
+                  value={transaction.destinationAmountDetails.transactionAmount}
+                  thousandSeparator
+                  decimalScale={2}
+                  prefix={`${transaction.destinationAmountDetails.transactionCurrency} `}
+                />
+              </Text>
+            </Box>
+          </Flex>
+
+          {/* <Divider my="sm" /> */}
+          {/* <Tabs variant="outline" defaultValue="originDevice"> */}
+          {/* <Tabs.List>
+              <Tabs.Tab value="originDevice">Origin Device</Tabs.Tab>
+              <Tabs.Tab value="destinationDevice">Destination Device</Tabs.Tab>
+            </Tabs.List> */}
+          <Flex direction="column" gap={20} mt={20}>
+            <Flex>
+              <SegmentedControl
+                data={[
+                  { value: 'originDevice', label: 'Origin Device' },
+                  { value: 'destinationDevice', label: 'Destination Device' },
+                ]}
+                radius={100}
+                value={activeTab}
+                onChange={setActiveTab}
+                color="blue"
+              />
+            </Flex>
+
+            {/* <Tabs.Panel value="originDevice" pt="xs"> */}
+
+            <Paper bg="gray.1" p={20} radius={20} w="80%">
+              {activeTab === 'originDevice'
+                ? renderDeviceDetails(transaction.originDeviceData)
+                : renderDeviceDetails(transaction.destinationDeviceData)}
+            </Paper>
+            {/* </Tabs.Panel> */}
+            {/* <Tabs.Panel value="destinationDevice" pt="xs"> */}
+            {/* </Tabs.Panel> */}
+            {/* </Tabs> */}
+          </Flex>
         </Flex>
       ) : (
-        <Text>Loading...</Text>
+        <Text size="sm" ta="right">
+          Loading...
+        </Text>
       )}
     </Drawer>
   );
